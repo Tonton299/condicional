bc6db5e702083a6892436fb16e181785648c7447
Walliston <tontongamer111@gmail.com>
2024-10-16 15:46:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// // Condicionais são uma forma de tomar decisões, de fazer o computador exucutar essas decisões.
+
+// // if significa "se"
+
+// // else significa "senão"
+
+// // if (condição) {codigo que será executado}
+
+// // variaveis
+
+// // 3 var, let e const
+
+// // prompt pergumta ao usuario
+
+// // sempre que usamos dados textuais devemos usar ""
+
+// // alert usamos para exibir uma mensagem
+
+// let idade = prompt("Quantos anos você tem?")
+// if significa "se"
+// else significa "senão"
+// if (condição) {codigo que sera executado}
+// if (idade >= 18) {
+//     alert("você ja pode tirar a carteira de motorista")
+// }
+// else{
+//     alert("Você ainda não tem idade para dirigir")
+// }
+
+let gostardDePizza = prompt("Você gosta de pizza? Responda (sim ou não)");
+// === estritamente igual
+if (gostardDePizza === "sim"){
+    alert("Que otimo! Vamos comer juntos")
+}else {
+    alert("Tudo bem, talvez você gosta de outra comida")
+}
